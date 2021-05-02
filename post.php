6f64ec37da6e8f401079ce2d41a38b13e072ceae
Ben Ringia <benringia@gmail.com>
2021-05-02 21:47:10 +0800

    show comments on posts based on their status

--- post.php
+++ post.php
@@ -87,44 +87,35 @@
                         </div>
                 <!-- Posted Comments -->
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
+                <?php 
+                    $query = "SELECT * FROM comments WHERE comment_post_id = {$postId} ";
+                    $query .= "AND comment_status = 'Approved' ";
+                    $query .= "ORDER BY comment_id DESC ";
+
+                    $selectCommentQuery = mysqli_query($dbConnect,$query);
+                    
+                    while($row = mysqli_fetch_assoc($selectCommentQuery)) {
+                        $commentDate = $row['comment_date'];
+                        $commentContent = $row['comment_content'];
+                        $commentAuthor = $row['comment_author'];
+                    ?>
+                      <!-- Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><?php echo $commentAuthor ?>
+                                <small><?php echo $commentDate ?></small>
+                            </h4>
+                            <?php echo $commentContent ?>
                         </div>
-                        <!-- End Nested Comment -->
                     </div>
-                </div>
+                        
+                   <?php } ?>
+              
+
 
     
                
