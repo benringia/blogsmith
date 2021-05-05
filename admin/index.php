18ce5a50c660f7573f27d02a177581fcd207050d
Ben Ringia <benringia@gmail.com>
2021-05-05 11:24:11 +0800

    display dynamic data on charts #1

--- admin/index.php
+++ admin/index.php
@@ -156,17 +156,25 @@
 
                     function drawChart() {
                         var data = google.visualization.arrayToDataTable([
-                        ['Year', 'Sales', 'Expenses', 'Profit'],
-                        ['2014', 1000, 400, 200],
-                        ['2015', 1170, 460, 250],
-                        ['2016', 660, 1120, 300],
-                        ['2017', 1030, 540, 350]
+                        ['Data', 'Count'],
+
+                        <?php 
+                            $elementText = ['Active Posts', 'Comments', 'Users', 'Categories'];
+                            $elementCount = [$postCount, $commentCount, $userCount, $categoryCount];
+
+                            for($i = 0; $i < 4; $i++) {
+                                echo "['$elementText[$i]'" . "," . "$elementCount[$i]],";
+                            }
+                        ?>
+
+                        // ['Posts', 1000]
+                       
                         ]);
 
                         var options = {
                         chart: {
-                            title: 'Company Performance',
-                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                            title: 'Post Performance',
+                            subtitle: 'Comments, Posts, and Users: 2021',
                         }
                         };
 
@@ -176,7 +184,7 @@
                     }
                     </script>
 
-<div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
                 </div>
             </div>
             <!-- /.container-fluid -->
