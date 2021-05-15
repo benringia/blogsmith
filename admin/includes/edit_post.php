f7176def2f861852a02466eb787e41ee5404db9e
Ben Ringia <benringia@gmail.com>
2021-05-15 17:11:35 +0800

    Update edit_post.php

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -152,7 +152,7 @@
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $postContent?></textarea>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo str_replace('\r\n','</br>',$postContent);?></textarea>
     </div>
 
  <div class="form-group">
